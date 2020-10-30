da727810b986b51e36eee34a931a58bc4f9d0258
Camille Neppy <dorumi714@gmail.com>
2020-10-30 12:11:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from indeed import extract_indeed_pages, extract_indeed_jobs
+
+last_indeed_pages = extract_indeed_pages()
+print(last_indeed_pages)
+
+indeef_jobs = extract_indeed_jobs(last_indeed_pages)
+print(extract_indeed_jobs)
