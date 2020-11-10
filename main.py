f99e8611d4ddb91212f6b572f7b35c10446514ae
Camille Neppy <dorumi714@gmail.com>
2020-11-10 07:41:33 +0000

    Extract Python Jobs and save it 'jobs.csv'

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from indeed import get_jobs as get_indeed_jobs
 from stackover import get_jobs as get_stackover_jobs
+from save import save_to_file
 
-# indeed_jobs = get_indeed_jobs()
+indeed_jobs = get_indeed_jobs()
 stackover_jobs = get_stackover_jobs()
 
-# print(indeed_jobs)
+jobs = stackover_jobs + indeed_jobs
+save_to_file(jobs)
--- stackover.py
+++ stackover.py
@@ -1,29 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = f"https://stackoverflow.com/jobs?q=python&pg=i"
+URL = f"https://stackoverflow.com/jobs?q=python"
 
 def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
   pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
-  last_page = pages[-2].get_text(strip=True)
+  last_page = pages[-2].get_text(strip=True)[0:1]
   return int(last_page)
 
 def extract_job(html):
   title = html.find("h2",{"class":"mb4"}).find("a")["title"]
   company, location = html.find("h3", {"class":"mb4"}).find_all("span", recursive=False)
-  print(company.get_text(strip=True), location.get_text(strip=True))
-  return {'title':title}
+  job_id = html.find("h2",{"class":"mb4"}).find("a")["href"]
+  # print(job_id) 
+  return {'Title':title,
+    'Company': company.get_text(strip=True), 
+    'Location': location.get_text(strip=True),
+    'apply_link': f"https://stackoverflow.com{job_id}"
+    }
+
 
 def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
+    print(f"Scrapping Stackover page {page+1}")
     result = requests.get(f"{URL}&pg={page+1}")
     soup = BeautifulSoup(result.text, "html.parser")
     results = soup.find_all("div", {"class":"grid--cell fl1"})
     for result in results:
       job = extract_job(result)
+      # print(job)
       jobs.append(job)
     return jobs
 
